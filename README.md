2a52a796bfe36de680cd7c4c95bdf26cbadc6766
Theodore Aptekarev <aptekarev@gmail.com>
2024-04-27 11:29:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Notebooks for working with GenAI audio
+
+## Installation
+
+1. Install from pypi
+
+   ```bash
+   pip install poetry
+   poetry install
+   ```
---
+++ tools/riffusion/__init__.py
@@ -0,0 +1,7 @@
+"""Riffusion library helpers.
+
+This module contains the helper classes and functions copied from
+the Riffusion library.
+
+https://github.com/riffusion/riffusion
+"""
